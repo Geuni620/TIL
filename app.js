930721e39eb86fe869ea7e0703d0210264b82f31
Geuni <dlrmsgnl620@naver.com>
2022-06-01 17:08:34 +0900

    Test commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const str = String(123);
+
+console.log(str, typeof str); // 123 string
+
+const num = Number("123");
+console.log(num, typeof num); // 123 number
+
+const bool = Boolean("true");
+console.log(bool, typeof bool); // true boolean
