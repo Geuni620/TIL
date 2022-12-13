40ae45ad2cf5c0bce0727cc95035ef0a38000951
Geuni <dlrmsgnl620@naver.com>
2022-12-14 01:15:15 +0900

    Refactor: 상태 점검: 다형성을 활용하여 데이터 생성하기 ~p77

--- Refactoring/1-1/createStatementData.js
+++ Refactoring/1-1/createStatementData.js
@@ -1,61 +1,25 @@
 export default function createStateMentData(invoice, plays) {
-  // 중간 데이터 생성을 전담
   const result = {};
   result.customer = invoice.customer;
   result.performances = invoice.performances.map(enrichPerformance);
   result.totalAmount = totalAmount(result);
   result.totalVolumeCredits = totalVolumeCredits(result);
-
   return result;
 
   function enrichPerformance(aPerformance) {
+    const calculator = createPerformanceCalculator(
+      aPerformance,
+      playFor(aPerformance)
+    );
     const result = Object.assign({}, aPerformance);
-    result.play = playFor(result); // 중간 데이터에 연극 정보를 저장
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
+    result.play = playFor(result);
+    result.amount = calculator.amount;
+    result.volumeCredits = calculator.volumeCredits;
     return result;
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance) {
-    let result = 0;
-
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40_000;
-
-        if (aPerformance.audience > 30) {
-          result += 1_000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30_000;
-
-        if (aPerformance.audience > 20) {
-          result += 10_000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-
-      default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-    }
-
-    return result;
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-
-    if ("comedy" === aPerformance.play.type) {
-      result += Math.floor(aPerformance.audience / 5);
-    }
-
-    return result;
   }
 
   function totalAmount(data) {
@@ -64,5 +28,67 @@ export default function createStateMentData(invoice, plays) {
 
   function totalVolumeCredits(data) {
     return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
+  }
+}
+
+function createPerformanceCalculator(aPerformance, aPlay) {
+  switch ((aPlay, type)) {
+    case "tragedy":
+      return new TragedyCalculator(aPerformance, aPlay);
+
+    case "comedy":
+      return new ComedyCalculator(aPerformance, aPlay);
+
+    default:
+      throw new Error(`알 수 없는 장르 : ${aPlay.type}`);
+  }
+}
+
+class PerformanceCalculator {
+  // 공연료 계산기 클래스
+  constructor(aPerformance, aPlay) {
+    this.performances = aPerformance;
+    this.play = aPlay;
+  }
+
+  get amount() {
+    throw new Error("서브 클래스에서 처리하도록 설계되었습니다.");
+  }
+
+  get volumeCredits() {
+    return Math.max(this.performances.audience - 30, 0);
+  }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    // amountFor() 함수의 코드를 계산기 클래스로 복사
+    let result = 40000;
+    if (this.performances.audience > 30) {
+      result += 1000 * (this.performances.audience - 30);
+    }
+
+    return result;
+  }
+
+  get volumeCredits() {
+    return Math.max(this.performances.audience - 30, 0);
+  }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    // amountFor() 함수의 코드를 계산기 클래스로 복사
+    let result = 30000;
+    if (this.performances.audience > 20) {
+      result += 1000 + 500 * (this.performances.audience - 20);
+    }
+
+    result += 300 * this.performances.audience;
+    return result;
+  }
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performances.audience / 5);
   }
 }
